cb2e859c5abe50f9e1127ac2ddd503613522e0f4
Fiifikev <kevinaryeh02@gmail.com>
2024-03-01 08:52:35 -0600

    memory and string len

--- mem.c
+++ mem.c
@@ -3,7 +3,7 @@
  *  * shell_mem - frees all the memory
  *   * @cmd: shell command
  *    * Return: 0
- *     */
+ */
 void shell_mem(char **cmd)
 {
 size_t i;
@@ -22,6 +22,25 @@ i++;
 if (!cmd[i])
 free(cmd[i]);
 free(cmd);
+}
 
+/**
+ *_strcat - Function that concatenates two strings.
+ *@destination: destination for concatenation
+ *@source: an input string
+ *Return: pointer to the resulting string
+ */
+char *_strcat(char *destination, char *source)
+{
+char *buff;
+buff = destination;
 
+while (*destination)
+destination++;
+/**the destination will determine the copy concatenation*/
+
+*destination++ = '/';
+while (*source)
+*destination++ = *source++;
+return (buff);
 }
