fc0d54d21278857a4234caa272b8a6837f2a1249
Fiifikev <kevinaryeh02@gmail.com>
2024-03-01 09:38:37 -0600

    exit shell

--- exit.c
+++ exit.c
@@ -19,3 +19,34 @@ status = _atoi(cmd[1]);
 shell_mem(cmd);
 exit(status);
 }
+
+
+
+/**
+ *free_shell - free all the memory allocated and exit
+ *@cmd: pointer to command memory
+ *Return: 0
+ */
+
+void free_shell(char **cmd)
+{
+size_t i = 0;
+
+if (!cmd)
+return;
+
+while (cmd[i])
+{
+free(cmd[i]);
+i++;
+}
+
+if (!cmd[i])
+free(cmd[i]);
+free(cmd);
+exit(EXIT_FAILURE);
+}
+
+
+
+
